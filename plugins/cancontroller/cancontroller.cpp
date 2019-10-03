2d2d4a846e0e0d02e825b5e6abfe40eab1c7721f
Sergio Espinoza <sergio.espinoza.lopez@gmail.com>
2019-10-03 16:41:37 -0500

    add can controller plugin for CAN configuration and CAN message monitoring

---
+++ plugins/cancontroller/cancontroller.cpp
@@ -0,0 +1,297 @@
+/**
+* YAZAKI COPYRIGHT
+*/
+
+
+#include <QtSerialBus/QCanBus>
+#include <QtSerialBus/QCanBusDevice>
+#include <QtSerialBus/QCanBusFrame>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QJsonArray>
+#include <QDebug>
+
+
+
+#include "cancontroller.h"
+
+
+
+CanController::CanController(QObject *parent)
+    : QObject(parent)
+{
+
+    //loads available pluginsw
+     m_canBus = QCanBus::instance();
+
+}
+
+
+int CanController::canInit( QString devName, int bitrate )
+{
+    int ret = CANCONTROLLER_ERROR;
+    QString errorMsg;
+    QList< QCanBusDeviceInfo > deviceList;
+    QCanBusDevice* canBusDevice;
+
+
+    if( m_canBus->plugins().contains( QStringLiteral( "socketcan" ) ) )
+    {
+        deviceList = m_canBus->availableDevices("socketcan", &errorMsg );
+
+       if( errorMsg.isEmpty() )
+       {
+           //TODO: check for devnName present in deviceList
+           canBusDevice = m_canBus->createDevice( "socketcan", devName, &errorMsg );
+
+           if( errorMsg.isEmpty() )
+           {
+               m_canBusDevices.insert( devName, canBusDevice );
+
+               qDebug() << "CAN bus device " + devName + " created";
+
+               connect(canBusDevice, &QCanBusDevice::errorOccurred,
+                       this,  &CanController::devErrorOccurred );               
+               connect(canBusDevice, &QCanBusDevice::framesReceived,
+                       this, &CanController::devFramesReceived );
+               connect(canBusDevice, &QCanBusDevice::framesWritten,
+                       this, &CanController::devFramesWritten );
+
+               canBusDevice->setConfigurationParameter( QCanBusDevice::BitRateKey, bitrate );
+
+               ret = CANCONTROLLER_SUCESS;
+           }
+           else
+           {
+               qDebug() << errorMsg;
+               qDebug() << "unable to create CAN " + devName + " device";
+           }
+       }
+       else
+       {
+           qDebug() << errorMsg;
+           qDebug() << "CAN device name " + devName + " not found";
+
+       }
+
+
+    }
+    else
+    {
+        qDebug() << "socket can plugin unavailable";
+
+        ret = CANCONTROLLER_ERROR;
+
+    }
+
+    return ret;
+
+}
+
+
+void CanController::devFramesReceived( void )
+{
+    QMap<QString, QCanBusDevice* >::iterator i;
+    QCanBusDevice* canDev;
+    QCanBusFrame frame;
+    QList< QCanBusFrame >::iterator cacheIterator;
+    bool idMatch = false;
+
+
+    // check for received frames on all buses
+    for( i = m_canBusDevices.begin() ; i != m_canBusDevices.end() ; ++i  )
+    {
+        canDev = i.value();
+
+        while( ( canDev->state() == QCanBusDevice::ConnectedState ) &&
+               (  canDev->framesAvailable() > 0 ) )
+        {
+            //unqueue frame by frame
+            frame = canDev->readFrame( );
+
+            if( ( frame.isValid() ) && ( frame.error() == QCanBusFrame::NoError ) )
+            {
+
+                idMatch = false;
+                //look for previous frame
+                for( cacheIterator =  m_messageCache[ i.key() ].begin() ;
+                     cacheIterator != m_messageCache[ i.key() ].end() ; ++cacheIterator )
+                {
+                    if( cacheIterator->frameId() == frame.frameId() )
+                    {
+                        //msg found compare to last recorded value
+                        idMatch = true;
+                        if( cacheIterator->payload() != frame.payload()  )
+                        {
+                            //update frame
+                            *cacheIterator = frame;
+                            emit rxMessageDataChanged( i.key(), frame );
+                        }
+                    }
+                }
+
+                //first time receive, add to cache, emit signal as well
+                if( !idMatch )
+                {
+                    m_messageCache[i.key()].append( frame );
+
+                    emit rxMessageDataChanged( i.key(), frame );
+
+                }
+
+            }
+        }
+
+    }
+}
+
+
+void CanController::devFramesWritten( qint64 framesWritten )
+{
+    Q_UNUSED( framesWritten )
+}
+
+
+void CanController::devErrorOccurred( QCanBusDevice::CanBusError error )
+{
+
+    //TODO: translate error and log into file
+    qDebug() << "CAN bus error !! ";
+
+
+    qDebug() << error;
+
+}
+
+
+int CanController::readCanConfigFile( QString devName, QString filename )
+{
+    int ret = CANCONTROLLER_ERROR;
+    QJsonParseError error;
+    QFile jsonFile( filename );
+    QByteArray fileData;
+
+    if( jsonFile.open(QIODevice::ReadOnly ) )
+    {
+        qWarning() << "Unable to open filter definition file";
+
+        fileData = jsonFile.readAll();
+
+        QJsonDocument jDoc = QJsonDocument::fromJson( fileData, &error ) ;
+
+        if( error.error != QJsonParseError::NoError )
+        {
+            //something went wrong during JSON parse
+            qWarning() << " Error during json filter definition file parse! ";
+            qWarning() << " error: " + error.errorString();
+        }
+        else
+        {
+
+        // get all ids
+            QJsonObject jObj = jDoc.object();
+
+            if( jObj.contains( "messages" ) )
+            {
+                ret = parseJsonMsgObj( devName, jObj );
+            }
+        }
+
+        if( ret == CANCONTROLLER_SUCESS )
+        {
+            //set own filters filters
+            setFilters( devName, m_canFilters[devName] );
+        }
+    }
+
+    return ret;
+
+}
+
+
+Q_INVOKABLE int CanController::initCanRx( QString devName )
+{
+    QCanBusDevice* canDev = nullptr;
+    int ret = CANCONTROLLER_ERROR;
+
+    if( m_canBusDevices.contains( devName ) )
+    {
+        canDev = m_canBusDevices[ devName ];
+
+        if( canDev->connectDevice() )
+        {
+            ret = CANCONTROLLER_SUCESS;
+        }
+        else
+        {
+            qWarning() << "unable to connect CAN " + devName + " device";
+
+        }
+
+    }
+
+    return  ret;
+}
+
+int CanController::parseJsonMsgObj ( QString devName, QJsonObject obj )
+{
+    int ret = CANCONTROLLER_ERROR;
+    QList<QCanBusDevice::Filter> filters;
+    QCanBusDevice::Filter tmpFilter;
+
+    Q_ASSERT( devName != "" );
+
+
+    QJsonArray jArr = obj[ "messages" ].toArray();
+
+
+    // ONLY CONFIGURE FILTERS FOR NOW
+
+    for( int i = 0 ; i < jArr.size() ; i++ )
+    {
+        if( obj.contains( "id" ) )
+        {
+             tmpFilter.frameId = (quint32)obj[ "id" ].toInt();
+             //only listen to specific id
+             tmpFilter.frameIdMask = 0x3FF;
+             tmpFilter.type = QCanBusFrame::DataFrame;
+
+             filters.append( tmpFilter );
+
+             //at least one message configured
+             ret = CANCONTROLLER_SUCESS;
+
+        }
+        else
+        {
+            //not possible to parse message
+            qDebug( ) << "cannot parse message";
+        }
+
+    }
+
+    //TODO: below statement should store into
+    // return value rather that directly
+    m_canFilters.insert( devName, filters );
+
+    //TODO: Implement complete message object parsing (name, cycle, signals, signal offsets etc.)
+
+    return ret;
+}
+
+
+
+int CanController::setFilters( QString devName, QList< QCanBusDevice::Filter >& filters )
+{
+    int ret = CANCONTROLLER_ERROR;
+    QCanBusDevice* canDev = nullptr;
+
+    if( ( canDev = m_canBusDevices[ devName ] ) != nullptr )
+    {
+        canDev->setConfigurationParameter( QCanBusDevice::RawFilterKey, QVariant::fromValue( filters ) );
+        ret = CANCONTROLLER_SUCESS;
+    }
+
+    return ret;
+}
+
---
+++ plugins/cancontroller/cancontroller.h
@@ -0,0 +1,215 @@
+/**
+* YAZAKI COPYRIGHT
+*/
+
+
+#ifndef CANCONTROLLER_H_
+#define CANCONTROLLER_H_
+
+#include <QtCore/QObject>
+#include <QQueue>
+#include <QList>
+#include <QMap>
+#include <QtSerialBus/QCanBus>
+#include <QtSerialBus/QCanBusDevice>
+
+
+#define CANCONTROLLER_SUCESS   0
+#define CANCONTROLLER_ERROR    -1
+
+/*!
+ * \brief The CanController class
+ */
+
+class CanController : public QObject
+{
+    Q_OBJECT
+
+public:
+    explicit CanController(QObject *parent = nullptr);
+
+
+    /*!
+     * \brief CanController::canInit
+     * \param devName
+     * \param bitrate
+     * \return
+     */
+    Q_INVOKABLE int canInit( QString devName, int bitrate );
+
+    /*!
+     * \brief CanController::readCanConfigFile
+     *   for now, only set filters according to defined messages
+     * \param devName
+     * \param filename
+     * \return
+     */
+    Q_INVOKABLE int readCanConfigFile( QString devName, QString filename );
+
+
+    /*!
+     * \brief initCanRx
+     * clears can buffer / queue, set filters
+     * filters for the given channel. Case no filters
+     * are set all messages will be listened to
+     * and will generate rxDataChanged /
+     * rxSignalValueChanged
+     *
+     *
+     * \return
+     */
+    Q_INVOKABLE int initCanRx( QString devName );
+
+
+    /*!
+     * \brief setFilters
+     *
+     * set up filters for given CAN channel
+     *
+     * \param devName
+     * \param filters
+     * \return
+     */
+     Q_INVOKABLE int setFilters(QString devName, QList<QCanBusDevice::Filter> &filters );
+
+
+signals:
+
+    /*!
+     * \brief rxMessageDataChanged
+     *  message payload related to given id changed from the last
+     *  received
+     * \param devName
+     * \param frame
+     */
+    void rxMessageDataChanged( QString devName, QCanBusFrame frame );
+
+
+    /*!
+     * \brief rxSignalValueChanged
+     * rx singal value changed from the last received
+     *
+     * \param signalName signal name
+     * \param value signal value
+     *
+     * TODO: Implement!!!
+     */
+    void rxSignalValueChanged( QString signalName, quint32 value);
+
+private slots:
+
+    /*!
+     * \brief devFramesReceived
+     * slot for QCanBusDevice::framesReceived
+     * frames received on active CanBusDevice
+     */
+    void devFramesReceived( void );
+
+    /*!
+     * \brief devFramesWritten
+     * frames written on active CanBusDevice
+     */
+    void devFramesWritten( qint64 framesWritten );
+
+    /*!
+     * \brief devErrorOccurred
+     * error occurred on active CanBusDevice
+     */
+    void devErrorOccurred( QCanBusDevice::CanBusError error );
+
+
+private:
+
+
+
+    /*!
+     * \brief initCanTx
+     * Initiate can message transmit thread.
+     * Tx Thread will start cycling tx msg transmission according
+     * to preset message cycles
+     *
+     * TODO: Implement!!
+     *
+     * \return
+     */
+    int initCanTx( QString devName );
+
+
+    /*!
+     * \brief parseJsonMsgObj
+     * read message parameters and set m_canFilters
+     * and others applicable members
+     * \param devName  channel nam
+     * \param obj  JSON object
+     *
+     * \return CANCONTROLLER_SUCCESS / FAIL
+     */
+    int parseJsonMsgObj ( QString devName, QJsonObject obj );
+
+
+
+
+    /*!
+     * \brief m_signalCache
+     * Holds last known signals values
+     * for each configured channel
+     *
+     * format is: QMap<"channel name" , QMap< "signal name", "value" > >
+     * TODO: To be implemented
+     */
+     QMap< QString, QMap< QString, int > > m_signalCache;
+
+     /**
+      * @brief m_messageCache
+      * Holds last known message values for each configured channel
+      *
+      * format is: QMap< "channel name", QList< "CAN messages" > >
+      *
+      */
+     QMap< QString, QList<QCanBusFrame> > m_messageCache;
+
+
+     /*!
+      * \brief m_canBus
+      *
+      *   can bus singleton
+      */
+     QCanBus* m_canBus;
+
+     /*!
+      * \brief m_canBusDevices
+      * Can bus devices, format:
+      * QMap< "device name", 'Bus device"  >
+      */
+
+     QMap< QString, QCanBusDevice* > m_canBusDevices;
+
+     /*!
+      * \brief m_canFilters
+      *
+      * filters currently associated with each channel.
+      *
+      * format is: QMap<"channel name", QList< "list of filters" > >
+      *
+      */
+     QMap< QString, QList<QCanBusDevice::Filter> > m_canFilters;
+
+     /*!
+      * \brief m_rxQueue
+      * stores received can frames ( ** MIGHT NOT BE NEEDED!!!** )
+      */
+     QQueue< QCanBusFrame > m_rxQueue;
+
+
+     /*!
+      * \brief m_txQueue
+      *  cyclic messages can id and data
+      * ( beware of asynch access by tx thread! )
+      * TODO: implement for cyclic messages
+      */
+     QQueue< QCanBusFrame > m_txQueue;
+
+
+};
+
+#endif // CANCONTROlLER_H_
